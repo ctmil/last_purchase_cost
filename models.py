26aa5d043de8deb757bdd47d57f4c7f9ee0295c1
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-20 14:55:17 -0300

    First commit

---
+++ models.py
@@ -0,0 +1,15 @@
+from openerp import models, fields, api, _
+from openerp.osv import osv
+from openerp.exceptions import except_orm, ValidationError
+from StringIO import StringIO
+import urllib2, httplib, urlparse, gzip, requests, json
+import openerp.addons.decimal_precision as dp
+import logging
+import datetime
+from openerp.fields import Date as newdate
+
+#Get the logger
+_logger = logging.getLogger(__name__)
+
+class purchase_order(models.Model):
+	_inherit = 'purchase.order'
