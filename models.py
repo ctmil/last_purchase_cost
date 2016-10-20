b0ff5255afb5b652545be6e5a0326276bc819227
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-20 16:10:56 -0300

    Updates pricelist with last price

--- models.py
+++ models.py
@@ -13,3 +13,22 @@ _logger = logging.getLogger(__name__)
 
 class purchase_order(models.Model):
 	_inherit = 'purchase.order'
+
+	@api.onchange('state')
+	def _check_state(self):
+		if self.state in ['purchase','done']:
+			import pdb;pdb.set_trace()
+			for line in self.order_line:
+				pricelist_id = self.env['product.supplierinfo'].search([\
+					('name','=',self.partner_id.id),\
+					('product_tmpl_id','=',self.product_id.product_tmpl_id.id)])
+				vals = {
+					'name': self.partner_id,
+					'product_tmpl_id': self.product_id.product_tmpl_id.id,
+					'min_qty': 0,
+					'price': line.price_unit
+					}
+				if not pricelist_id:
+					pricelist_id = self.env['product.supplierinfo'].create(vals)
+				else:
+					pricelist_id.write(vals)
