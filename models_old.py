c0a39e6a9fc7c59dc51988efcf44c4bc68ea5adc
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-24 19:50:53 -0300

    Added taxes_id to purchase_requistion POL creation process

--- models_old.py
+++ models_old.py
@@ -14,7 +14,14 @@ class purchase_requisition(osv.osv):
 	def _prepare_purchase_order_line(self, cr, uid, requisition, requisition_line, purchase_id, supplier, context=None):
 		res = super(purchase_requisition, self)._prepare_purchase_order_line(\
 			cr,uid,requisition,requisition_line,purchase_id,supplier,context)
-		import pdb;pdb.set_trace()
+		taxes_ids = res.get('taxes_id',False)
+		if taxes_ids:
+			if taxes_ids[0][2] == []:
+				product_id = res.get('product_id',False)
+				if product_id:
+					product = self.pool.get('product.product').browse(cr,uid,product_id)
+					if product.supplier_taxes_id:
+						res['taxes_id'] = [(6,0,product.supplier_taxes_id.ids)])
 		return res
 
 purchase_requisition()
