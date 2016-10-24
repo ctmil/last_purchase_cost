f87d20d603fdfdafff8f05078ad0d4197cc73123
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-24 17:42:38 -0300

    Fixed bug

--- models.py
+++ models.py
@@ -21,8 +21,7 @@ class purchase_order(models.Model):
 		if purchase_state in ['draft','purchase','done']:
 			order_line = vals.get('order_line',False)
 			for line in order_line:
-				import pdb;pdb.set_trace()
-				product_id = line['product_id']
+				product_id = line[0][2]['product_id']
 				pricelist_id = self.env['product.supplierinfo'].search([\
 					('name','=',self.partner_id.id),\
 					('product_tmpl_id','=',product_id.product_tmpl_id.id)])
@@ -30,14 +29,14 @@ class purchase_order(models.Model):
 					'name': self.partner_id.id,
 					'product_tmpl_id': product_id.product_tmpl_id.id,
 					'min_qty': 0,
-					'price': line['price_unit']
+					'price': line[0][2]['price_unit']
 					}
 				if not pricelist_id:
 					pricelist_id = self.env['product.supplierinfo'].create(vals)
 				else:
 					pricelist_id.write(vals)
 				vals_product_tmpl = {
-					'standard_price': line['price_unit']
+					'standard_price': line[0][1]['price_unit']
 					}
 				product_tmpl = product_id.product_tmpl_id
 				product_tmpl.write(vals_product_tmpl)
